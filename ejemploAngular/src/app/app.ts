e701707ce6272933f6c523fa69dee645db16fee3
ANUAR-LAPTOP\anuar <akhan@decroly.com>
2025-10-20 16:08:38 +0200

    Instalacion de bootStrap y ejemplos

--- ejemploAngular/src/app/app.ts
+++ ejemploAngular/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer-component/footer-component';
 import { BodyComponent } from "./components/body-component/body-component";
+import { NavBar } from "./components/nav-bar/nav-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, BodyComponent],
+  imports: [FooterComponent, BodyComponent, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
